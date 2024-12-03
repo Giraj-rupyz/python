927ddd1cac3f9e56e9c67581d22382eb65b38dea
Giraj-rupyz <giraj.neema@rupyz.com>
2024-12-03 12:24:05 +0530

    numeric data type

---
+++ python_numbers.py
@@ -0,0 +1,22 @@
+# In python there are 3 types of numeric data types
+# 1. Integers
+# 2. floating point numbers
+# 3. complex numbers
+
+x=3e4 # e is the scientific notation it adds number of zeros which is specified after e prefix.
+print(x)
+
+y= 3e-4 # it adds zeros to left side
+print(y)
+
+
+# integer can be as long as possible
+x=5*(10**18)
+print(x)
+
+# complex numbers
+# it consist of two parts real and imaginary
+
+z=5+10j #imaginary part is written with a suffix j or J
+print(z.real)
+print(z.imag)

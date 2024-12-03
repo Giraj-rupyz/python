446d355280516430a0c53701ef769f1cef8d308a
Giraj-rupyz <giraj.neema@rupyz.com>
2024-12-03 11:13:31 +0530

    arithmetic-operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,22 @@
+x=5
+y=10
+# addition
+print(x+y)
+
+#subtraction
+print(x-y)
+
+#multiplication
+print(x*y)
+
+#division
+print(x/y)
+
+# modulus
+print(x%y)
+
+#exponential
+print(y**x)
+
+#floor division
+print(x//y)
